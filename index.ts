ee66d3b1728c14453afaa20a6df078f6e8eaf7b4
Asamanli <saman.ali1911@gmail.com>
2024-07-15 03:52:25 +0500

    creating countdown timer with typescript

---
+++ index.ts
@@ -0,0 +1,43 @@
+#! /usr/din/env node
+
+import {differenceInSeconds} from "date-fns";
+import inquirer from "inquirer";
+
+
+const res = await inquirer.prompt([
+    {
+        name: "response",
+        type: "number",
+        message: "How many seconds coutdown is required:",
+        validate: (input)=>{
+            if(isNaN(input)){
+                return "please enter valid number";
+            }else if(input > 60){
+                return "seconds must be in 60";
+            }else{
+                return true;
+            }
+        }
+    }
+]);
+
+
+//to fire after each second
+function startTime(val:number){
+    const initialTime = new Date().setSeconds(new Date().getSeconds()+val);
+    const intervalTime = new Date(initialTime);
+    setInterval(()=>{
+        const currentTime = new Date();
+        const timeDiff = differenceInSeconds(intervalTime, currentTime);
+        if(timeDiff <= 0){
+            console.log("Timer hass expired");
+            process.exit();
+        }
+        const minute = Math.floor((timeDiff%(3600*24))/3600);
+        const seconds = Math.floor(timeDiff%60);
+        console.log(`${minute.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`);
+    },1000);
+}
+
+startTime(res.response);
+
